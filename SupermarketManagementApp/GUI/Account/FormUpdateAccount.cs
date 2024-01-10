ee2f0596176f96e2c8642f8d8b540fb41919f8e9
DungLe2983 <lequocdung2983@gmail.com>
2024-01-10 14:54:13 +0700

    CRUD Account

--- SupermarketManagementApp/GUI/Account/FormUpdateAccount.cs
+++ SupermarketManagementApp/GUI/Account/FormUpdateAccount.cs
@@ -1,4 +1,8 @@
-﻿using SupermarketManagementApp.Properties;
+﻿using SupermarketManagementApp.BUS;
+using SupermarketManagementApp.DTO;
+using SupermarketManagementApp.GUI.Employee;
+using SupermarketManagementApp.Properties;
+using SupermarketManagementApp.Utils;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,15 +12,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace SupermarketManagementApp.GUI.Account
 {
     public partial class FormUpdateAccount : Form
     {
-        public FormUpdateAccount()
+        private DTO.Account account;
+        private AccountBUS accountBUS;
+        EmployeeBUS employeeBUS = null;
+        private List<string> listEmployeeID = new List<string>();
+        private FormAccountManagement formAccountManagement;
+        public FormUpdateAccount(FormAccountManagement formAccountManagement, int id)
         {
             InitializeComponent();
+            accountBUS = AccountBUS.GetInstance();
+            employeeBUS = EmployeeBUS.GetInstance();
+            this.formAccountManagement = formAccountManagement;
             txtBoxPassword.IconRightClick += txtBoxPassword_IconRightClick;
+            loadAccount(id);
+           
+        }
+        private async void loadAccount(int _id)
+        {
+            await loadEmployeeData();
+            Result<DTO.Account> result = await accountBUS.getAccountByID(_id);
+            account = new DTO.Account();
+            if (result.IsSuccess)
+            {
+                this.account.AccountID = result.Data.AccountID;
+                this.cbBoxEmployeeID.Text = result.Data.EmployeeID.ToString();
+                this.cbBoxRole.Text = result.Data.Role;
+                this.txtBoxUsername.Text = result.Data.Username;
+                this.txtBoxPassword.Text = result.Data.Password;
+               
+            }
+            else
+            {
+                MessageBox.Show("Load employee failed!!");
+                this.Close();
+            }
+        }
+
+        private async Task loadEmployeeData()
+        {
+            Result<IEnumerable<DTO.Employee>> result = await employeeBUS.GetAllEmployee();
+            if (result.IsSuccess)
+            {
+                cbBoxEmployeeID.DataSource = result.Data;
+                cbBoxEmployeeID.DisplayMember = "EmployeeID"; // Hiển thị thuộc tính "Ten" của đối tượng
+                cbBoxEmployeeID.ValueMember = "EmployeeID";    // Sử dụng thuộc tính "Id" của đối tượng khi chọn mục
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
@@ -24,9 +70,22 @@ namespace SupermarketManagementApp.GUI.Account
             this.Close();
         }
 
-        private void btnSave_Click(object sender, EventArgs e)
+        private async void btnSave_Click(object sender, EventArgs e)
         {
-            this.Close();
+            account.Role = cbBoxRole.Text;
+            account.Username= txtBoxUsername.Text;
+            account.Password= txtBoxPassword.Text;
+            Result<DTO.Account> result = await accountBUS.updateAccount(account);
+            if (result.IsSuccess)
+            {
+                MessageBox.Show("Update Account successfully!");
+                this.formAccountManagement.InitAllAccount();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
         }
 
         private void txtBoxPassword_IconRightClick(object sender, EventArgs e)
